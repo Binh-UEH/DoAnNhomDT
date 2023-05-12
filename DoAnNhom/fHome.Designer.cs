a2cee8aaac23401ee1bdc95fc25d55bbdf4ca26d
31211027571@ueh.edu.vn <31211027571@ueh.edu.vn>
2023-05-12 22:11:39 +0700

    Add project files.

---
+++ DoAnNhom/fHome.Designer.cs
@@ -0,0 +1,396 @@
+﻿namespace DoAnNhom
+{
+    partial class fHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnMenu = new System.Windows.Forms.Panel();
+            this.btnDangXuat = new System.Windows.Forms.Button();
+            this.btnTTTK = new System.Windows.Forms.Button();
+            this.btnThanhToan = new System.Windows.Forms.Button();
+            this.btnLinhKien = new System.Windows.Forms.Button();
+            this.btnCamera = new System.Windows.Forms.Button();
+            this.btnLap = new System.Windows.Forms.Button();
+            this.btnPC = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnMenu.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnMenu
+            // 
+            this.pnMenu.Controls.Add(this.btnDangXuat);
+            this.pnMenu.Controls.Add(this.btnTTTK);
+            this.pnMenu.Controls.Add(this.btnThanhToan);
+            this.pnMenu.Controls.Add(this.btnLinhKien);
+            this.pnMenu.Controls.Add(this.btnCamera);
+            this.pnMenu.Controls.Add(this.btnLap);
+            this.pnMenu.Controls.Add(this.btnPC);
+            this.pnMenu.Location = new System.Drawing.Point(3, 3);
+            this.pnMenu.Name = "pnMenu";
+            this.pnMenu.Size = new System.Drawing.Size(1260, 38);
+            this.pnMenu.TabIndex = 0;
+            // 
+            // btnDangXuat
+            // 
+            this.btnDangXuat.Location = new System.Drawing.Point(1106, 4);
+            this.btnDangXuat.Name = "btnDangXuat";
+            this.btnDangXuat.Size = new System.Drawing.Size(151, 31);
+            this.btnDangXuat.TabIndex = 4;
+            this.btnDangXuat.Text = "Đăng xuất";
+            this.btnDangXuat.UseVisualStyleBackColor = true;
+            // 
+            // btnTTTK
+            // 
+            this.btnTTTK.Location = new System.Drawing.Point(949, 4);
+            this.btnTTTK.Name = "btnTTTK";
+            this.btnTTTK.Size = new System.Drawing.Size(151, 31);
+            this.btnTTTK.TabIndex = 3;
+            this.btnTTTK.Text = "Thông tin tài khoản";
+            this.btnTTTK.UseVisualStyleBackColor = true;
+            // 
+            // btnThanhToan
+            // 
+            this.btnThanhToan.Location = new System.Drawing.Point(631, 4);
+            this.btnThanhToan.Name = "btnThanhToan";
+            this.btnThanhToan.Size = new System.Drawing.Size(151, 31);
+            this.btnThanhToan.TabIndex = 3;
+            this.btnThanhToan.Text = "Thanh toán";
+            this.btnThanhToan.UseVisualStyleBackColor = true;
+            // 
+            // btnLinhKien
+            // 
+            this.btnLinhKien.Location = new System.Drawing.Point(474, 4);
+            this.btnLinhKien.Name = "btnLinhKien";
+            this.btnLinhKien.Size = new System.Drawing.Size(151, 31);
+            this.btnLinhKien.TabIndex = 3;
+            this.btnLinhKien.Text = "Linh kiện";
+            this.btnLinhKien.UseVisualStyleBackColor = true;
+            // 
+            // btnCamera
+            // 
+            this.btnCamera.Location = new System.Drawing.Point(317, 4);
+            this.btnCamera.Name = "btnCamera";
+            this.btnCamera.Size = new System.Drawing.Size(151, 31);
+            this.btnCamera.TabIndex = 2;
+            this.btnCamera.Text = "Camera";
+            this.btnCamera.UseVisualStyleBackColor = true;
+            // 
+            // btnLap
+            // 
+            this.btnLap.Location = new System.Drawing.Point(160, 4);
+            this.btnLap.Name = "btnLap";
+            this.btnLap.Size = new System.Drawing.Size(151, 31);
+            this.btnLap.TabIndex = 1;
+            this.btnLap.Text = "Laptop";
+            this.btnLap.UseVisualStyleBackColor = true;
+            // 
+            // btnPC
+            // 
+            this.btnPC.Location = new System.Drawing.Point(3, 4);
+            this.btnPC.Name = "btnPC";
+            this.btnPC.Size = new System.Drawing.Size(151, 31);
+            this.btnPC.TabIndex = 0;
+            this.btnPC.Text = "PC";
+            this.btnPC.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.pictureBox6);
+            this.panel2.Controls.Add(this.pictureBox7);
+            this.panel2.Controls.Add(this.pictureBox8);
+            this.panel2.Controls.Add(this.pictureBox9);
+            this.panel2.Controls.Add(this.pictureBox10);
+            this.panel2.Controls.Add(this.pictureBox5);
+            this.panel2.Controls.Add(this.pictureBox4);
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(27, 99);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1209, 429);
+            this.panel2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(988, 380);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 31);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "SP1";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(738, 380);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(147, 31);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "SP1";
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(523, 380);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(147, 31);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "SP1";
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(316, 380);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(147, 31);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "SP1";
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(81, 380);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(147, 31);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "SP1";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(988, 177);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(147, 31);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "SP1";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(738, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(147, 31);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "SP1";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(523, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 31);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "SP1";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(316, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 31);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "SP1";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(81, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 31);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "SP1";
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(985, 219);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(150, 142);
+            this.pictureBox6.TabIndex = 8;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(520, 219);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(150, 142);
+            this.pictureBox7.TabIndex = 7;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(313, 219);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(150, 142);
+            this.pictureBox8.TabIndex = 5;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(735, 219);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(150, 142);
+            this.pictureBox9.TabIndex = 6;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Location = new System.Drawing.Point(79, 219);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(150, 142);
+            this.pictureBox10.TabIndex = 4;
+            this.pictureBox10.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(985, 20);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(150, 142);
+            this.pictureBox5.TabIndex = 3;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(520, 20);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(150, 142);
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(313, 20);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(150, 142);
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(735, 20);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(150, 142);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(79, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 142);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // fHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1272, 543);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pnMenu);
+            this.Name = "fHome";
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.Home_Load);
+            this.pnMenu.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnMenu;
+        private System.Windows.Forms.Button btnTTTK;
+        private System.Windows.Forms.Button btnThanhToan;
+        private System.Windows.Forms.Button btnLinhKien;
+        private System.Windows.Forms.Button btnCamera;
+        private System.Windows.Forms.Button btnLap;
+        private System.Windows.Forms.Button btnPC;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnDangXuat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ DoAnNhom/fTTTK.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace DoAnNhom
+{
+    partial class fTTTK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnDangXuat = new System.Windows.Forms.Button();
+            this.btnTTTK = new System.Windows.Forms.Button();
+            this.btnThanhToan = new System.Windows.Forms.Button();
+            this.btnLinhKien = new System.Windows.Forms.Button();
+            this.btnCamera = new System.Windows.Forms.Button();
+            this.btnLap = new System.Windows.Forms.Button();
+            this.btnPC = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnDangXuat);
+            this.panel1.Controls.Add(this.btnTTTK);
+            this.panel1.Controls.Add(this.btnThanhToan);
+            this.panel1.Controls.Add(this.btnLinhKien);
+            this.panel1.Controls.Add(this.btnCamera);
+            this.panel1.Controls.Add(this.btnLap);
+            this.panel1.Controls.Add(this.btnPC);
+            this.panel1.Location = new System.Drawing.Point(0, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1260, 38);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnDangXuat
+            // 
+            this.btnDangXuat.Location = new System.Drawing.Point(1106, 4);
+            this.btnDangXuat.Name = "btnDangXuat";
+            this.btnDangXuat.Size = new System.Drawing.Size(151, 31);
+            this.btnDangXuat.TabIndex = 4;
+            this.btnDangXuat.Text = "Đăng xuất";
+            this.btnDangXuat.UseVisualStyleBackColor = true;
+            // 
+            // btnTTTK
+            // 
+            this.btnTTTK.Location = new System.Drawing.Point(949, 4);
+            this.btnTTTK.Name = "btnTTTK";
+            this.btnTTTK.Size = new System.Drawing.Size(151, 31);
+            this.btnTTTK.TabIndex = 3;
+            this.btnTTTK.Text = "Thông tin tài khoản";
+            this.btnTTTK.UseVisualStyleBackColor = true;
+            // 
+            // btnThanhToan
+            // 
+            this.btnThanhToan.Location = new System.Drawing.Point(631, 4);
+            this.btnThanhToan.Name = "btnThanhToan";
+            this.btnThanhToan.Size = new System.Drawing.Size(151, 31);
+            this.btnThanhToan.TabIndex = 3;
+            this.btnThanhToan.Text = "Thanh toán";
+            this.btnThanhToan.UseVisualStyleBackColor = true;
+            // 
+            // btnLinhKien
+            // 
+            this.btnLinhKien.Location = new System.Drawing.Point(474, 4);
+            this.btnLinhKien.Name = "btnLinhKien";
+            this.btnLinhKien.Size = new System.Drawing.Size(151, 31);
+            this.btnLinhKien.TabIndex = 3;
+            this.btnLinhKien.Text = "Linh kiện";
+            this.btnLinhKien.UseVisualStyleBackColor = true;
+            // 
+            // btnCamera
+            // 
+            this.btnCamera.Location = new System.Drawing.Point(317, 4);
+            this.btnCamera.Name = "btnCamera";
+            this.btnCamera.Size = new System.Drawing.Size(151, 31);
+            this.btnCamera.TabIndex = 2;
+            this.btnCamera.Text = "Camera";
+            this.btnCamera.UseVisualStyleBackColor = true;
+            // 
+            // btnLap
+            // 
+            this.btnLap.Location = new System.Drawing.Point(160, 4);
+            this.btnLap.Name = "btnLap";
+            this.btnLap.Size = new System.Drawing.Size(151, 31);
+            this.btnLap.TabIndex = 1;
+            this.btnLap.Text = "Laptop";
+            this.btnLap.UseVisualStyleBackColor = true;
+            // 
+            // btnPC
+            // 
+            this.btnPC.Location = new System.Drawing.Point(3, 4);
+            this.btnPC.Name = "btnPC";
+            this.btnPC.Size = new System.Drawing.Size(151, 31);
+            this.btnPC.TabIndex = 0;
+            this.btnPC.Text = "PC";
+            this.btnPC.UseVisualStyleBackColor = true;
+            // 
+            // fTTTK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1272, 543);
+            this.Controls.Add(this.panel1);
+            this.Name = "fTTTK";
+            this.Text = "TTTK";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnDangXuat;
+        private System.Windows.Forms.Button btnTTTK;
+        private System.Windows.Forms.Button btnThanhToan;
+        private System.Windows.Forms.Button btnLinhKien;
+        private System.Windows.Forms.Button btnCamera;
+        private System.Windows.Forms.Button btnLap;
+        private System.Windows.Forms.Button btnPC;
+    }
+}
